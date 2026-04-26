4fb9db917265b9347a1aeb720ff255749b778ea4
jsuriv <surivillarpandojhonathan@gmail.com>
2026-04-26 19:31:03 -0400

    hola

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Bienvenido a la API de Nocturne Cold Storage"', () => {
+      expect(appController.getHello()).toBe('Bienvenido a la API de Nocturne Cold Storage');
     });
   });
 });
