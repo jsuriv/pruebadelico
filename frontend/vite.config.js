89e32ff0e1dcbf755a76a885573357a98adddb67
jsuriv <surivillarpandojhonathan@gmail.com>
2026-04-26 19:36:10 -0400

    una

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: '../dist/client',
+    emptyOutDir: true
+  },
   server: {
     port: 3000,
     proxy: {
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'frontend', 'dist'),
+      rootPath: join(__dirname, '..', 'dist', 'client'),
     }),
   ],
   controllers: [AppController],
