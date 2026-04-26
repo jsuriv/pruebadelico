07dacc92042a4c83b7a0e3d3886f7b9ca2ed2f37
jsuriv <surivillarpandojhonathan@gmail.com>
2026-04-26 19:33:29 -0400

    todo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001); // Puerto cambiado a 3001 para dejar libre el 3000 al frontend
+  await app.listen(process.env.PORT ?? 3000); // Puerto 3000, NestJS servirá el frontend
 }
 bootstrap();
